c13b843d63cf7019074694cc5500cc5fecca54a7
sandrabosk <sandraboskovic@hotmail.com>
2020-06-11 21:43:49 -0400

    add lesson code for react forms demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    firstName: 'Jose',
+    email: '',
+    message: ''
+  };
+
+  sendMessage = event => {
+    event.preventDefault();
+    const data = {
+      firstName: this.state.firstName,
+      email: this.state.email,
+      message: this.state.message
+    };
+    alert(`Message with data: ${JSON.stringify(data, null, 2)} was sent.`);
+  };
+
+  changeName = event => {
+    const value = event.target.value;
+    this.setState({
+      firstName: value
+    });
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const inputName = event.target.name;
+    this.setState({
+      [inputName]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <form onSubmit={this.sendMessage}>
+          <input
+            type='text'
+            placeholder='Name'
+            value={this.state.firstName}
+            name='firstName'
+            onChange={this.changeName}
+            // onChange={this.handleInputChange}
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
+          <textarea placeholder='Message' name='message' value={this.state.message} onChange={this.handleInputChange} />
+          <span>Message being sent by {this.state.firstName}</span>
+          <button>Send Message</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
